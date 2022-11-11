94a42029f53c716e07530839f509aaab88513eb8
UJ ⸌⍤⃝⸍ <87402295+LuCky0324z@users.noreply.github.com>
2022-11-11 21:57:29 +0800

    Create dfcode.cpp

---
+++ dfcode.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define MustGoFast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define INF 0x3f3f3f3f
+
+using namespace std;
+using pii=pair<int,int>;
+using pll=pair<long long,long long>;
+using pdd=pair<long double,long double>;
+using tiii=tuple<int,int,int>;
+
+
+int main(){
+    MustGoFast;
+    
+    return 0;
+}
